a7027ea4b29737565bf90255d5e355099be80b26
Igor Fernandes <igorkuivjogi@hotmail.com>
2021-07-07 22:42:18 -0300

    create validate_packages_at_pypi function

--- README.md
+++ README.md
@@ -5,8 +5,24 @@ Ative o ambiente virtual:
 pipenv shell
 ```
 
+Instale as biblicas:
+```bash
+pipenv install
+```
+
 Comando para rodar os testes:
 ```bash
 coverage run --source . manage.py test
 coverage report -m
+```
+
+Rode as migrações:
+```bash
+python3 manage.py makemigrations
+python3 manage.py migrate
+```
+
+Inicie a aplicação:
+```bash
+python3 manage.py runserver
 ```
--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,48 @@
 from rest_framework import serializers
+import requests
 
 from .models import PackageRelease, Project
+
+
+def validate_packages_at_pypi(project: dict):
+    '''
+    Faz requisições na API do PyPi para verificar se cada pacote do projeto é valido.   
+    Caso a versão do pacote não seja especificada no projeto, então é capturada a versão
+    mais recente disponível na API do PyPi.  
+    É possível que projeto não use nenhum pacote (ou seja, "packages": []), então
+    neste caso não é necessário fazer requisições na API do PyPi.
+
+    Exemplo:
+    >>> project = {
+        'name': 'titan',
+        'packages': [
+            {'name': 'Django'},
+            {'name': 'pypypypypypy', 'version': '2.0'},
+            {'name': 'graphene', 'version': '2.0'},
+        ]
+    }
+    >>> validate_packages_at_pypi(project)
+    >>> ({'error': 'One or more packages do not exist.'}, 400)
+    '''
+
+    STATUS_CODE = 201
+
+    for package in project['packages']:
+        project_name = package.get('name', ' ')
+        url = f'https://pypi.org/pypi/{project_name}/json'
+        response = requests.get(url)
+
+        try:
+            response.raise_for_status()
+            pypi_data = response.json()['info']
+            package['name'] = pypi_data['name']
+            package['version'] = package.get('version', pypi_data['version'])
+        except requests.exceptions.HTTPError:
+            project = {'error': 'One or more packages do not exist.'}
+            STATUS_CODE = 400
+            break
+
+    return project, STATUS_CODE
 
 
 class PackageSerializer(serializers.ModelSerializer):
